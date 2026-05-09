546c13f05ba80356d4037d9fb7cc207aa74a73f7
Erfan Tabatabaei <erfantabatabaei1380@gmail.com>
2026-05-09 05:28:22 -0400

    v.0.1.3: fixing __all__ in __init__.py and publish version 3

--- src/pyros_performance/__init__.py
+++ src/pyros_performance/__init__.py
@@ -1,5 +1,5 @@
-from .profiler import profiler
+from .profiler import profiler, render
 from importlib.metadata import version
 
-__all__ = ["profiler"]
+__all__ = ["profiler", "render"]
 __version__ = version("pyros-performance")
